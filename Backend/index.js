a6c6fc3f3ba7b4a7382f68488df4d25be059d361
Nikhil Naik <46309727+nikhilsurfingaus@users.noreply.github.com>
2023-01-05 14:28:57 +1100

    intial commit

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const ytdl = require('ytdl-core')
+const cors = require('cors')
+
+
+const app = express()
+app.use(cors())
+
+app.get("/download", async(req, res) => {
+    try {
+        const url = req.query.url;
+        const videoId = await ytdl.getURLVideoID(url);
+        const metaInfo = await ytdl.getInfo(url);
+
+        let data = {
+            url: "https://www.youtube.com/embed/" + videoId,
+            info: metaInfo.formats
+        }
+
+        return res.send(data)
+    } catch (error) {
+        return res.status(500)
+    }
+})
+
+app.listen(4000, () => {
+    console.log("Server is running on PORT: 4000")
+})
